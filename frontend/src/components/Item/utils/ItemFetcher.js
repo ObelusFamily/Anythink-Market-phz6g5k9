d3bd204af1a54be01d97fd69c01c9dbc22ef2e78
guyzion <guyzion7@gmail.com>
2023-12-29 19:26:30 +0000

    rtp333tewwgt

--- frontend/src/components/Item/utils/ItemFetcher.js
+++ frontend/src/components/Item/utils/ItemFetcher.js
@@ -4,5 +4,7 @@ export async function getItemAndComments(id) {
   const item = agent.Items.get(id);
   const comments = agent.Comments.forItem(id);
 
-  return Promise.all([item, comments]);
+  const [it, cm] = await Promise.all([item, comments]);
+  return [it, cm];
+
 }
